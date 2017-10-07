38a9b318c2d64a499476229ff5d3864ebd4391b4
Franco Victorio <victorio.franco@gmail.com>
2017-10-07 15:12:52 -0300

    Add jest and a basic test suite

---
+++ tests/lib.spec.js
@@ -0,0 +1,47 @@
+const { stripIndent } = require('common-tags')
+const { injectDependencies } = require('../lib/lib')
+
+describe('lib', () => {
+  describe('injectDependencies', () => {
+    const code = stripIndent`
+      const answer = 42
+
+      console.log(answer)
+    `.trim();
+
+    test('no dependencies', () => {
+      const dependencies = {};
+      const result = injectDependencies(dependencies, code)
+
+      expect(result).toMatchSnapshot()
+    })
+
+    test('one dependency', () => {
+      const dependencies = {
+        lodash: '_'
+      };
+      const result = injectDependencies(dependencies, code)
+
+      expect(result).toMatchSnapshot()
+    })
+
+    test('two dependencies', () => {
+      const dependencies = {
+        lodash: '_',
+        trae: 'trae'
+      };
+      const result = injectDependencies(dependencies, code)
+
+      expect(result).toMatchSnapshot()
+    })
+
+    test('local dependency', () => {
+      const dependencies = {
+        './lib': 'lib'
+      };
+      const result = injectDependencies(dependencies, code)
+
+      expect(result).toMatchSnapshot()
+    })
+  })
+});
